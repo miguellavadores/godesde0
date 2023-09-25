b7f8106664efc8123b57309be5a4ee6924246e0d
Miguel Angel Lavadores <miguel.lavadores@grupofiglobal.com>
2023-09-25 16:31:22 -0600

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mlavadores/godesde0
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mlavadores/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
